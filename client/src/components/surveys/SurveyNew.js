0d9ca73f47c280633eaa62e1f7cbdbbed8493ad9
Robert Newman <bobs-n@hotmail.com>
2020-06-30 17:07:58 +0100

    Survey form and form input validation

---
+++ client/src/components/surveys/SurveyNew.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import SurveyForm from './SurveyForm';
+
+class SurveyNew extends Component {
+    render(){
+        return(
+            <div>
+                <h1>New Survey</h1>
+                <SurveyForm />
+            </div>
+        )
+    }
+}
+
+export default SurveyNew;
