6f2d14f9a78b0fa5f7922361f4e8533804c026c7
Robert Newman <bobs-n@hotmail.com>
2020-07-13 13:05:00 +0100

    Display list of active surveys

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_USER } from './types'
+import { FETCH_USER, FETCH_SURVEYS } from './types'
 
 // Check out the fetchUser() commented out function below to see the EXACT same function without ES2015 syntax
 export const fetchUser = () => async dispatch => {
@@ -16,6 +16,12 @@ export const submitSurvey = (formValues, history) => async dispatch => {
     const res = await axios.post('/api/surveys', formValues)
     history.push('/surveys')
     dispatch({type: FETCH_USER, payload: res.data})
+    
+}
+
+export const fetchSurveys = () => async dispatch => {
+    const res = await axios.get('/api/surveys');
+    dispatch({type: FETCH_SURVEYS, payload: res.data});
     
 }
 
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,15 +1,25 @@
-const mongoose = require('mongoose')
-const requireLogin = require('../middlewares/requireLogin')
-const requireCredits = require('../middlewares/requireCredits')
+const mongoose = require('mongoose');
+const _ = require("lodash");
+const { Path } = require('path-parser');
+const { URL } = require('url');
+const requireLogin = require('../middlewares/requireLogin');
+const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
+const keys = require('../config/keys');
 
 const Survey = mongoose.model('surveys');
 
 module.exports = app =>{
 
-    app.get('/api/surveys/thanks', (req, res) => {
+    app.get('/api/surveys/:surveyId/:choice', (req, res) => {
         res.send("THANKS FOR VOTING");
+    });
+
+    app.get('/api/surveys', requireLogin, async (req, res) => {
+        const surveys = await Survey.find({ _user:req.user.id })
+            .select({recipients: false});
+        res.send(surveys);
     });
 
     app.post('/api/surveys', requireLogin, requireCredits, async (req, res)=>{
@@ -23,7 +33,7 @@ module.exports = app =>{
             // Take a string of emails, split into an array and return a list of each email as an object
             recipients: recipients.split(',').map(email => ({ email: email.trim() })),
             _user: req.user.id,
-            dateSent: Date.now()
+            dateSend: Date.now()
         });
 
         // New Email
@@ -39,4 +49,61 @@ module.exports = app =>{
         }
         
     });
+
+    // See app.post below for more detailed description of how this function operates
+    app.post('/api/surveys/webhooks', (req, res) => {
+        const p = new Path('/api/surveys/:surveyId/:choice');
+        // The lodash .chain method allows us to chain other methods to the array (which is req.body in this instance)
+        _.chain(req.body)
+            .map(({url, email}) => {
+                const match = p.test(new URL(url).pathname);
+                if(match) {
+                    return {email, ...match };
+                } 
+            })
+            .compact()
+            .uniqBy('email', 'surveyId')
+            .each(({surveyId, email, choice}) => {
+                Survey.updateOne({
+                    _id: surveyId,
+                    recipients: {
+                        // Matches specific elements on a related field, recipients is a many to 1 relationship (kinda, as mongoDb is non-relational)
+                        $elemMatch: {email: email, responded: false}
+                    }
+                }, {
+                    // $inc increments a key by the value : {key: value to increment by}
+                    $inc: {[choice]: 1},
+                    // $set will set a key to the value, the $ here represents the result of the above query so the key is:
+                    // recipients.<personWhoMatchedFromAboveQuery>.responded
+                    $set: {'recipients.$.responded': true},
+                    lastResponded: new Date()
+                }).exec();
+            })
+            .value();
+
+        res.send({});
+    });
 };
+
+
+// app.post('/api/surveys/webhooks', (req, res) => {
+//     // Sendgrid will use this API to send a list of events that it knows about
+//     const events = _.map(req.body, ({url, email}) => {
+//         // next line gets the url path (everything after mywebsite.com/ -> so it returns: /api/surveys/<surveyId>/yes)
+//         const pathName = new URL(url).pathname
+//         // The next 2 lines will strip the url and return stuff inside the /:surveyId and /:choice part of the url inside of an object with these keys. (eg. {surveyId: 12, choice: "yes"} )
+//         const p = new Path('/api/surveys/:surveyId/:choice');
+//         const match = p.test(pathName);
+//         // Merge the email with the returned "match" object
+//         if(match) {
+//             return {email, ...match };
+//         } 
+//     });
+
+//     // removes all undefined values in the object (defensive code for someone doing something like removing the ID in the url)
+//     const compactEvents = _.compact(events);
+//     // Ensures that each record has a unique combination of email against surveyId (someone cant vote twice on the same survey);
+//     const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+//     console.log(uniqueEvents);
+//     res.send({});
+// });
